122af361a822503c8f47b8d2ec127207ce5c284e
acer <acer@DESKTOP-QDHOQ5E>
2021-03-10 22:06:43 +0300

    10 Mart commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './componets/product/product.component';
+import { CategoryComponent } from './componets/category/category.component';
+import { NaviComponent } from './componets/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
