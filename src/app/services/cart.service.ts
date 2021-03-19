5a652fbdc965e8a9e17a98c17690d816173fc007
acer <acer@DESKTOP-QDHOQ5E>
2021-03-19 15:48:21 +0300

    19 Mart Commit

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CartItem } from '../models/cartItem';
-import { CartItems } from '../models/CartItems';
+import { CartItems } from '../models/cartItems';
 import { Product } from '../models/product';
 
 @Injectable({
