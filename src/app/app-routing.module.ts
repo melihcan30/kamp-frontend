bb5653afa605ef1590374d57ab8abdea0fa4176f
acer <acer@DESKTOP-QDHOQ5E>
2021-03-20 23:53:53 +0300

    20 Mart Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductAddComponent } from './componets/product-add/product-add.component';
 import { ProductComponent } from './componets/product/product.component';
 
 const routes: Routes = [
   {path:"",pathMatch:"full", component:ProductComponent},
   {path:"products", component:ProductComponent},
-  {path:"products/category/:categoryId", component:ProductComponent}
+  {path:"products/category/:categoryId", component:ProductComponent},
+  {path:"products/add", component:ProductAddComponent}
 ];
 
 @NgModule({
